71218895a895c5162d9e283bc185e8039ba523c3
Utkp5 <utkp200@gmail.com>
2022-08-26 17:28:01 +0530

    adding functionality

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ let btns = btn.querySelectorAll(".item");
 for(let i = 0; i<btns.length; i++ )
 {
     btns[i].addEventListener("click", function () {
-        console.log(this.innerHTML);
-        // alert(this.innerText);
-        result.value = result.value + this.innerText;
+        if (this.innerHTML == "=") {
+            result.value = eval(result.value);
+        } 
+        else if (this.innerHTML == "x") {
+            result.value = result.value + "*"; 
+        }
+        else {
+            
+            result.value = result.value + this.innerText;
+        }
     });
 }
 
 
 let clrtext = document.getElementById("clear_text").addEventListener("click", function () {
  result.value = " ";
-});
+});  //ye hm else me bhi likh skte hai is else laga ke uper me
